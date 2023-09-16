f15621e87f2d522322da195f74f20c3a81d16cce
coderseyfi <seyfaddinmn@code.edu.az>
2023-09-16 23:46:59 +0400

    first-component

---
+++ src/components/Todo.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const  Todos: React.FC = (props) => {
+  return (
+    <ul>{
+        
+        }</ul>
+  )
+}
+
+export default Todos
+
+//TODO React.FC stands for function component also type her
