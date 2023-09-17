fec7319911bc1e24032755fe5d3edabb2a964bd9
coderseyfi <seyfaddinmn@code.edu.az>
2023-09-17 19:07:28 +0400

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import './App.css'
-import Todos from './components/Todo'
+import { useState } from 'react'
+
+import NewTodo from './components/NewTodo'
+import Todos from './components/Todos'
+import Todo from './models/todo'
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText)
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo)
+    })
+  }
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId)
+    })
+  }
+
   return (
     <div>
-      <Todos />
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   )
 }
